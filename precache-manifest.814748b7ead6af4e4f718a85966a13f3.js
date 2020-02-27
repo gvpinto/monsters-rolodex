dcc63a42830b57fc14c3cd63337ad2e887062a71
Glenn Pinto <gvpinto@gmail.com>
2020-02-26 21:11:54 -0500

    Updates

---
+++ precache-manifest.814748b7ead6af4e4f718a85966a13f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecd86a490f61f8d769c257707800bd09",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dac63bd8068347f414ee",
+    "url": "/monsters-rolodex/static/css/main.359e811f.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dac63bd8068347f414ee",
+    "url": "/monsters-rolodex/static/js/main.c88f68d6.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
